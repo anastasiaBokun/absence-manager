149a3f48375d404579edb29f25fb7bd9749769f5
Anastasia Bokun <nastya.bokun@gmail.com>
2022-03-15 09:57:21 +0100

    F3 Added formatted for DataGrid columns.html

--- src/components/AbsenceGrid/AbsenceGrid.tsx
+++ src/components/AbsenceGrid/AbsenceGrid.tsx
@@ -1,13 +1,73 @@
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import type { ExtendedAbsence } from '../DataLayer/types';
+import Tooltip from '@mui/material/Tooltip';
+import Chip from '@mui/material/Chip';
+import SickIcon from '@mui/icons-material/Sick';
+import LocalAirportIcon from '@mui/icons-material/LocalAirport';
+import { Note } from './styles';
 
 const columns: GridColDef[] = [
-  { field: 'memberName', headerName: 'Name', width: 150 },
-  { field: 'type', headerName: 'Type of Absence', width: 150 },
+  { field: 'memberName', headerName: 'Name', width: 100, minWidth: 80, flex: 1 },
+  {
+    field: 'type',
+    headerName: 'Type of Absence',
+    width: 150,
+    minWidth: 60,
+    flex: 1,
+    renderCell: (params) => {
+      const icon =
+        params.value === 'sickness' ? (
+          <SickIcon />
+        ) : params.value === 'vacation' ? (
+          <LocalAirportIcon />
+        ) : undefined;
+      return <Chip label={params.value} icon={icon} variant="outlined" />;
+    }
+  },
   { field: 'period', headerName: 'Period', width: 200 },
-  { field: 'memberNote', headerName: 'Member Note', width: 250 },
-  { field: 'status', headerName: 'Status', width: 150 },
-  { field: 'admitterNote', headerName: 'Admitter note', width: 250 }
+  {
+    field: 'memberNote',
+    headerName: 'Member Note',
+    width: 300,
+    minWidth: 100,
+    flex: 2,
+    renderCell: (params) => (
+      <Note>
+        <Tooltip title={params.value}>
+          <span>{params.value}</span>
+        </Tooltip>
+      </Note>
+    )
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    width: 150,
+    minWidth: 100,
+    flex: 1,
+    renderCell: (params) => {
+      const color =
+        params.value === 'Requested'
+          ? 'primary'
+          : params.value === 'Confirmed'
+          ? 'success'
+          : 'error';
+      return <Chip label={params.value} color={color} variant="outlined" />;
+    }
+  },
+  {
+    field: 'admitterNote',
+    headerName: 'Admitter note',
+    minWidth: 100,
+    flex: 2,
+    renderCell: (params) => (
+      <Note>
+        <Tooltip title={params.value}>
+          <span>{params.value}</span>
+        </Tooltip>
+      </Note>
+    )
+  }
 ];
 
 interface DataTableProps {
